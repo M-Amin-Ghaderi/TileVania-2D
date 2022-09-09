54f4cceaa77d3d056682d75f1c524dc14ef46b03
M_Amin_Ghaderi <amingh0079@gmail.com>
2022-09-09 13:17:01 +0430

    Running animation Added
    I added Running Animation And Jumping Function not its Animation Yet

--- TileVania 2D/Assets/Scripts/Player.cs
+++ TileVania 2D/Assets/Scripts/Player.cs
@@ -7,21 +7,26 @@ using UnityStandardAssets.CrossPlatformInput;
 public class Player : MonoBehaviour
 {
     [SerializeField] float runSpeed = 10f;
-
+    [SerializeField] float jumpForce = 5f;
 
     private Rigidbody2D rb2d;
+    private Animator anim;
+    private Collider2D mycollider2D;
 
 
     // Start is called before the first frame update
     void Start()
     {
         rb2d = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+        mycollider2D = GetComponent<Collider2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
         Run();
+        Jump();
         FlipSprite();
     }
     public void Run()
@@ -29,6 +34,19 @@ public class Player : MonoBehaviour
         float controlThrow = CrossPlatformInputManager.GetAxis("Horizontal");
         Vector2 playerVelocity = new Vector2(controlThrow * runSpeed, rb2d.velocity.y);
         rb2d.velocity = playerVelocity;
+        bool playerHasHorizontalSpeed = Mathf.Abs(rb2d.velocity.x) > math.EPSILON;
+        anim.SetBool("Running", playerHasHorizontalSpeed);
+
+    }
+
+    public void Jump()
+    {
+        if (!mycollider2D.IsTouchingLayers(LayerMask.GetMask("Ground"))) { return; }
+        if (CrossPlatformInputManager.GetButtonDown("Jump"))
+        {
+            Vector2 jumpingVeclocity = new Vector2(0f, jumpForce);
+            rb2d.velocity += jumpingVeclocity;
+        }
     }
 
     private void FlipSprite()
